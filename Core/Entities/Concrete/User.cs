13805073d31e5dea4d508736d6e721420d893c00
Ekin Köseoğlu <ekindenizkoseoglu@gmail.com>
2021-07-01 17:45:30 +0300

    HashingHelper Class
    Creating "HashingHelper" class in "Hashing" folder that belongs to "Utilities" folder in "Core" Layer. And created into it two method for both creating hash with salt algorithm and verify it.

--- Core/Entities/Concrete/User.cs
+++ Core/Entities/Concrete/User.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Core.Entities.Concrete
 {
-    public class User
+    public class User:IEntity
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public bool Status { get; set; }
     }
 }
---
+++ Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace Core.Utilities.Security.Hashing
+{
+    public class HashingHelper
+    {
+        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt) // Bu method girdiğimiz bir password'un Hash değerini ve Salt değerini oluşturmamıza yarıyor.
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512()) // Burada oluşturulan class için "hmac" diyeceğim. O "hmac aslında bizim kriptografide kullandığımız class'a (HMACSHA512) karşılık geliyor."
+            {
+                passwordSalt = hmac.Key; // İlgili kullandığımız algoritmanın o an oluşturduğu key değeridir. Her kullanıcı için başka bir Key oluşturur.
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)); // Hash'imiz de string tipindeki password'u byte değerinde göndermemizi istiyor.
+            }
+        }
+
+        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt) // Datadaki şifreylegirilen şifrenin Hashlerinin karşılaştırarak doğrulama yapıyor
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt)) // Aşağıdaki hashleme tamamen buradaki Salt algoritması kullanılarak yapıylıyor.
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)); // Bu password kullanıcının sonrasında girdiği parola, bunun eskisiyle eşleştirmek için tekrardan hash'ini yaratacağız (yukarıdaki Saltlama algoritmasını kullanarak)
+
+                for (int i = 0; i < computedHash.Length; i++) // İki hash'i karşılaştırıyoruz
+                {
+                    if (computedHash[i] != passwordHash[i]) // oluşan iki değer de byte[] dir 
+                    {
+                        return false;       // eşleşmezlerse uyumsuz parola
+                    }
+                }
+            }
+
+            return true; // Eşleşirlerse parola doğru
+        }
+    }
+}
